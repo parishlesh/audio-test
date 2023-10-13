0ae66660f41a6bdf7b933330b01418a6400ffbbd
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-13 09:37:58 +0530

    there is a bug, i'll fix It

---
+++ transcribe.py
@@ -0,0 +1,33 @@
+
+import speech_recognition as sr
+import pydub
+from pydub import AudioSegment
+from pydub.playback import play 
+
+print("start")
+AudioSegment.converter = "C:\\PATH_Programs\\ffmpeg.exe"
+# Convert mp3 file to wav
+
+print("start2")
+sound = AudioSegment.from_mp3("transcript.mp3")
+sound.export("transcript.wav", format="wav")
+print("start3")
+# Transcribe audio file
+AUDIO_FILE = "transcript.wav"
+
+# Use the audio file as the audio source
+r = sr.Recognizer()
+
+# Load the audio file
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # Read the entire audio file
+
+# Transcribe the audio
+transcription = r.recognize_google(audio)
+
+# Print the transcription
+print("Transcription: " + transcription)
+
+# Save the transcription to a text file
+with open("transcript.txt", "w") as text_file:
+    text_file.write(transcription)
