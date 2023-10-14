209a6fe7c9c3a1a091da9d079d1e3178e4caeb70
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-14 14:50:22 +0530

    added the login form component

--- audiotext/src/App.js
+++ audiotext/src/App.js
@@ -1,11 +1,13 @@
 import Body from "./components/Body";
 import Navbar from "./components/Navbar";
+import Loginform from "./components/Loginform";
 
 
 function App() {
   return (
    <>
     <Navbar/>
+    <Loginform/>
     <Body/>
     
    </>
