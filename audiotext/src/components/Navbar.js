ba65fb00e2bcb15a1f456de88c6d1f64fbfd419b
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-14 14:54:52 +0530

    added the signup button

--- audiotext/src/components/Navbar.js
+++ audiotext/src/components/Navbar.js
@@ -9,13 +9,20 @@ export default function Navbar() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown" >
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">About</a>
+                            </li>
+                        </ul>
+                        <ul className='signuplist'>
+                            <li>
+                                <button className='signupbutton'>
+                                    <span>Sign Up</span>
+                                </button>
                             </li>
                         </ul>
                     </div>
