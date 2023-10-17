6b443daede0eee1cdcc6e8e473a1614645a10ac0
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-17 14:36:22 +0530

    made the login page nicely

--- audiotext/src/components/Loginform.js
+++ audiotext/src/components/Loginform.js
@@ -1,32 +1,35 @@
-    import React, { useState } from 'react'
-    import '../styles/loginform.css'
+import React, { useState } from 'react'
+import '../styles/loginform.css'
 
 
 
-    export default function LoginForm() {
-        const [isLogin, setIsLogin] = useState(true)
+export default function LoginForm() {
+    const [isLogin, setIsLogin] = useState(true)
 
-        const handleSwitchClick = () => {
-            setIsLogin(!isLogin);
-        };
+    const handleSwitchClick = () => {
+        setIsLogin(!isLogin);
+    };
 
-        return (
+    return (
+        <div className='form'>
 
-            <div className='form'>
-                <div className='cover'>
-                    <div className="row">
-                        <img className="logo" src="logo.jpg" alt="logo logo"/>
-                            <h1>AppName</h1>
-                    </div>
-                    <h3>What's new?</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, quia
-                        nulla modi nostrum odio sequi repudiandae quasi qui possimus explicabo
-                        rerum iste cupiditate, vero ab facilis quibusdam a! Molestias, quasi!
-                    </p>
+           
+           {/* <div className='cover' style={isLogin ? {left: '15px'} :{ right: "15px"}}> */}
+           <div className={`cover ${isLogin ? 'toRight' : 'toLeft'}`}>
+                <div className="row">
+                    <img className="logo" src="logo.jpg" alt="logo logo" />
+                    <h1>AppName</h1>
                 </div>
+                <h3>What's signup new?</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, quia
+                    nulla modi nostrum odio sequi repudiandae quasi qui possimus explicabo
+                    rerum iste cupiditate, vero ab facilis quibusdam a! Molestias, quasi!
+                </p>
+            </div>
+           
 
-
+            <div style={{ display: 'flex' }}>
                 <div className='login-section center'>
 
                     <h1>Login</h1>
@@ -34,12 +37,11 @@
                     <input className='form-input' type='text'></input>
                     <label htmlFor=''>Password:</label>
                     <input className='form-input' type='password'></input>
-                    <button>Login</button>
+                    <button style={{ zIndex: '0' }}>Login</button>
                     <p>
                         Don't have account yet?
                         <span class="register-link" id="signup" onClick={handleSwitchClick}>Register</span>
                     </p>
-
                 </div>
                 <div className='signup-section center'>
 
@@ -50,7 +52,7 @@
                     <input className='form-input' type='password'></input>
                     <label htmlFor=''>Repeat Password:</label>
                     <input className='form-input' type='password-repeat'></input>
-                    <button>Sign Up</button>
+                    <button style={{ zIndex: '0' }}>Sign Up</button>
                     <p>
                         Already have an account yet?
                         <span class="register-link" id="signup" onClick={handleSwitchClick}>Login</span>
@@ -59,5 +61,6 @@
                 </div>
 
             </div>
-        )
-    }
+        </div>
+    )
+}
