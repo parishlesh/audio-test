ec444d0e6a8550c5cf4c19d467e3aa4f7a9b3247
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-17 15:51:59 +0530

    completed the login form

--- audiotext/src/components/Loginform.js
+++ audiotext/src/components/Loginform.js
@@ -11,10 +11,19 @@ export default function LoginForm() {
     };
 
     return (
-        <div className='form'>
+        <div>
+            <div className='closeButton'>
+                <button style={{all:"unset"}}>
 
-           
-           {/* <div className='cover' style={isLogin ? {left: '15px'} :{ right: "15px"}}> */}
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+                </button>
+
+            </div>
+
+        <div className='form'>
            <div className={`cover ${isLogin ? 'toRight' : 'toLeft'}`}>
                 <div className="row">
                     <img className="logo" src="logo.jpg" alt="logo logo" />
@@ -61,6 +70,7 @@ export default function LoginForm() {
                 </div>
 
             </div>
+        </div>
         </div>
     )
 }
