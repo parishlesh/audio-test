9735bfd300f6200b79bdedc6d5632c25bcfea97f
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-17 12:14:45 +0530

    made the login for and added the css

--- audiotext/src/components/Loginform.js
+++ audiotext/src/components/Loginform.js
@@ -1,23 +1,22 @@
-import React, {useState} from 'react'
-import '../styles/loginform.css'
+    import React, { useState } from 'react'
+    import '../styles/loginform.css'
 
 
 
-export default function LoginForm() {
-    const [isLogin, setIsLogin]= useState(true)
-    
-    const handleSwitchClick=()=>{
-        setIsLogin(!isLogin);
-    };
-    
-    return (
-        <div className='loginForm'>
-            <div className="form-section">
-                
-                <div className="switch" id="switch">
+    export default function LoginForm() {
+        const [isLogin, setIsLogin] = useState(true)
+
+        const handleSwitchClick = () => {
+            setIsLogin(!isLogin);
+        };
+
+        return (
+
+            <div className='form'>
+                <div className='cover'>
                     <div className="row">
-                        <img className="logo" src="logo.jpg" alt="logo logo" />
-                        <h1>AppName</h1>
+                        <img className="logo" src="logo.jpg" alt="logo logo"/>
+                            <h1>AppName</h1>
                     </div>
                     <h3>What's new?</h3>
                     <p>
@@ -26,34 +25,39 @@ export default function LoginForm() {
                         rerum iste cupiditate, vero ab facilis quibusdam a! Molestias, quasi!
                     </p>
                 </div>
-                {/* <div className="login-form center"> */}
-                <div className="login-form center" style={{ display: isLogin ? 'block' : 'none' }}>
-                    <h1>Sign Up</h1>
-                    <label htmlFor="">Email:</label>
-                    <input className="form-input" type="text" />
-                    <label htmlFor="">Password:</label>
-                    <input className="form-input" type="password" />
-                    <label htmlFor="">Password repeat:</label>
-                    <input className="form-input" type="password" />
-                    <button className="btn-switch">Sign Up</button>
-                    <p>
-                        Already have an account?<span className="register-link" id="login">Login</span>
-                    </p>
-                </div>
-                {/* <div className="signup-form center"> */}
-                <div className="signup-form center" style={{ display: isLogin ? 'none' : 'block' }}>
+
+
+                <div className='login-section center'>
+
                     <h1>Login</h1>
-                    <label htmlFor="">Email:</label>
-                    <input className="form-input" type="text" />
-                    <label htmlFor="">Password:</label>
-                    <input className="form-input" type="password" />
-                    <button className="btn-switch" onClick={handleSwitchClick}>{isLogin ? 'Switch to Signup' : 'Switch to Login'}</button>
+                    <label htmlFor=''>Email:</label>
+                    <input className='form-input' type='text'></input>
+                    <label htmlFor=''>Password:</label>
+                    <input className='form-input' type='password'></input>
+                    <button>Login</button>
                     <p>
-                        Don't have account yet?<span className="register-link" id="signup">Register</span>
+                        Don't have account yet?
+                        <span class="register-link" id="signup" onClick={handleSwitchClick}>Register</span>
                     </p>
+
                 </div>
+                <div className='signup-section center'>
+
+                    <h1>Sign Up</h1>
+                    <label htmlFor=''>Email:</label>
+                    <input className='form-input' type='text'></input>
+                    <label htmlFor=''>Password:</label>
+                    <input className='form-input' type='password'></input>
+                    <label htmlFor=''>Repeat Password:</label>
+                    <input className='form-input' type='password-repeat'></input>
+                    <button>Sign Up</button>
+                    <p>
+                        Already have an account yet?
+                        <span class="register-link" id="signup" onClick={handleSwitchClick}>Login</span>
+                    </p>
+
+                </div>
+
             </div>
-            {/* <script src="script.js"></script> */}
-        </div>
-    )
-}
+        )
+    }
